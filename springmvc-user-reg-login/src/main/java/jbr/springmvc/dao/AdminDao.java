72731044549772182763768186e7dc8a16811286
bhavanitoall <92046890+bhavanitoall@users.noreply.github.com>
2021-11-09 16:11:47 +0530

    Add files via upload

---
+++ springmvc-user-reg-login/src/main/java/jbr/springmvc/dao/AdminDao.java
@@ -0,0 +1,11 @@
+package jbr.springmvc.dao;
+
+//import jbr.springmvc.model.AdminLogin;
+//import jbr.springmvc.model.Login;
+//import jbr.springmvc.model.User;
+//
+//public interface AdminDao {
+//
+//	int register(AdminLogin adminlogin);
+//	AdminLogin validate(AdminLogin adminlogin);
+//}
